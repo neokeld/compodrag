949481efdba35184309c9f474da2fe4f1ba6988d
Arnaud Duforat <arnaud.duforat@capgemini.com>
2019-01-25 14:30:28 +0100

    evergreen: init react app with evergreen as a ui lib

---
+++ evergreen/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button } from 'evergreen-ui';
+
+class App extends Component {
+  render() {
+    return (
+      <Button>Evergreen React App</Button>
+    );
+  }
+}
+
+export default App;
