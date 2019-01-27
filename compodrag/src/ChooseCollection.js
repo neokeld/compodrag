94bdbaeb3232b4a04c268d6576b68fcf501cdc08
neokeld <arnaud.duforat@protonmail.ch>
2019-01-28 00:40:34 +0100

    responsive rows

--- compodrag/src/ChooseCollection.js
+++ compodrag/src/ChooseCollection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box } from 'grommet';
+import { Box, Image, Text } from 'grommet';
 
 export const ChooseCollection = () => (
 <>
@@ -8,35 +8,33 @@ export const ChooseCollection = () => (
 </Box>
 <Box fill>
    <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-     <Box flex align='center'>
-       Atlaskit
+     <Box flex width='medium' height='medium'>
+       <Image src='a.png' fit='contain' alt='atlaskit ui example'/>
+       <Text alignSelf='center'>Atlaskit</Text>
      </Box>
-     <Box flex align='center'
-       background='light-2'
-     >
-       <img src='blueprint_kit.gif' width='400'/>
-       <p>Blueprint</p>
+     <Box flex width='medium' height='medium' background='light-2'>
+       <Image src='blueprint_kit.gif' fit='contain' alt='blueprint ui example'/>
+       <Text alignSelf='center'>Blueprint</Text>
      </Box>
-     <Box flex align='center'>
-       Evergreen
+     <Box flex justify='center' width='medium' height='medium'>
+       <Image src='e.png' fit='contain' alt='evergreen ui example'/>
+       <Text alignSelf='center'>Evergreen</Text>
      </Box>
    </Box>
 </Box>
 <Box fill>
    <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-     <Box flex align='center'
-       background='light-2'
-	>
-       <img src='grommet-2-open-theme.png' width='400'/>
-       <p>Grommet</p>
+     <Box flex width='medium' height='medium' background='light-2'>
+       <Image src='grommet-2-open-theme.png' fit='contain' alt='grommet ui example'/>
+       <Text alignSelf='center'>Grommet</Text>
      </Box>
-     <Box flex align='center'>
-       Rebass
+     <Box flex width='medium' height='medium'>
+       <Image src='r.png' fit='contain' alt='rebass ui example'/>
+       <Text alignSelf='center'>Rebass</Text>
      </Box>
-     <Box flex align='center'
-       background='light-2'
-     >
-       Semantic-ui
+     <Box flex width='medium' height='medium' background='light-2'>
+       <Image src='s.png' fit='contain' alt='semantic ui example'/>
+       <Text alignSelf='center'>Semantic-ui</Text>
      </Box>
    </Box>
 </Box>
