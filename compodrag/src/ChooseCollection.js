59ec4a04f89e3a3cdd7d3f0d17623ad79cb34cbb
Arnaud Duforat <arnaud.duforat@capgemini.com>
2019-01-28 18:53:55 +0100

    compodrag: extract cell into a component

--- compodrag/src/ChooseCollection.js
+++ compodrag/src/ChooseCollection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Image, Text, RoutedAnchor } from 'grommet';
+import { Box } from 'grommet';
 
 import { CollectionCell } from './CollectionCell';
 
@@ -10,43 +10,17 @@ export const ChooseCollection = () => (
 </Box>
 <Box fill>
    <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-       <Box flex width='medium' height='medium'>
-         <Image src='a.png' fit='contain' alt='atlaskit ui example'/>
-         <RoutedAnchor path='/atlaskit'>
-	   <Text alignSelf='center'>Atlaskit</Text>
-     </RoutedAnchor>
-       </Box>
-       <Box flex width='medium' height='medium' background='light-2'>
-         <Image src='blueprint_kit.gif' fit='contain' alt='blueprint ui example'/>
-       
-     <RoutedAnchor path='/blueprint'>
-	<Text alignSelf='center'>Blueprint</Text>
-     </RoutedAnchor>
-	</Box>
-       <Box flex justify='center' width='medium' height='medium'>
-         <Image src='e.png' fit='contain' alt='evergreen ui example'/>
-     <RoutedAnchor path='/evergreen'>
-	<Text alignSelf='center'>Evergreen</Text>
-     </RoutedAnchor>
-	</Box>
+     <CollectionCell imgSrc='a.png' path='/atlaskit' name='Atlaskit' />
+     <CollectionCell imgSrc='blueprint_kit.gif' path='/blueprint' name='Blueprint' background='light-2' />
+     <CollectionCell imgSrc='e.png' path='/evergreen' name='Evergreen' />
    </Box>
 </Box>
 <Box fill>
    <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-       <Box flex width='medium' height='medium' background='light-2' onclick='() => { console.log("xd") }'>
-         <Image src='grommet-2-open-theme.png' fit='contain' alt='grommet ui example'/>
-         <RoutedAnchor path='/grommet'>
-	   <Text alignSelf='center'>Grommet</Text>
-         </RoutedAnchor>
-	</Box>
-       <Box flex width='medium' height='medium'>
-         <Image src='r.png' fit='contain' alt='rebass ui example'/>
-     <RoutedAnchor path='/rebass'>
-	<Text alignSelf='center'>Rebass</Text>
-     </RoutedAnchor>
-	</Box>
-     <CollectionCell />
-     </Box>
+     <CollectionCell imgSrc='grommet-2-open-theme.png' path='/grommet' name='Grommet' background='light-2' />
+     <CollectionCell imgSrc='r.png' path='/rebass' name='Rebass' />
+     <CollectionCell imgSrc='s.png' path='/semanticui' name='Semantic-ui' background='light-2' />
+   </Box>
 </Box>
 </>
 );
--- compodrag/src/CollectionCell.js
+++ compodrag/src/CollectionCell.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { Box, Image, Text, RoutedAnchor } from 'grommet';
 import { withRouter } from 'react-router-dom';
 
-export const CollectionCell = withRouter(({ history }) => (
-  <Box flex width='medium' height='medium' background='light-2' align='center'>
-    <Image src='s.png' fit='contain' alt='semantic ui example' onClick={() => {
-      history.push("/semanticui");
-      console.log("sem");
-    }} />
-    <RoutedAnchor path='/semanticui'>
-      <Text>Semantic-ui</Text>
+export const CollectionCell = withRouter(({ history, imgSrc, path, name, background }) => (
+  <Box flex width='medium' height='300px' background={ background }>
+    <Image src={ imgSrc } fit='contain' alt='{ name } example' onClick={ () => history.push(`${ path }`) } />
+    <RoutedAnchor path={ path } alignSelf='center'>
+      <Text>{ name }</Text>
     </RoutedAnchor>
   </Box>
 ));
